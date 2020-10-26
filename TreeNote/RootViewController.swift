3bd5897179ca3330570ed112ac66365657870473
Eugeniy <eugeniyloshchenko@gmail.com>
2020-10-26 14:58:44 +0300

    Initial commit

---
+++ TreeNote/RootViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RootViewController.swift
+//  UITest2
+//
+//  Created by Evgenii Loshchenko on 20.10.2020.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func test()
+    {
+        print("test")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
